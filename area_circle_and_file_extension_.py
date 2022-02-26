060b305d1d222f754ebef0727429199a3e4df53d
gittywitty12 <100278356+gittywitty12@users.noreply.github.com>
2022-02-26 16:44:28 +0530

    Update area_circle_and_file_extension_.py

--- area_circle_and_file_extension_.py
+++ area_circle_and_file_extension_.py
@@ -1,15 +1,24 @@
-from cmath import pi
+'''
 
+MyCaptain assignment 1
+author : Akshay kalyan
+date : 26-02-2022
+
+'''
+
+#1. printing the area of circle by taking radius as and input from user
+
+from cmath import pi
 
 radius=float(input("Input the radius of the circle: "))
 print(f"area of the circle {radius} is: \n", radius*radius*pi)
 
+#-------------------------------------------------------------------------------------------------------------------------------
+
+#2. program to print the file extension after acceptng the filename from user
 
 filename=input("enter the full name of file: ")
 file_and_ext=filename.split(".")
 if file_and_ext[1] == "py":
     print("the extension of the file is python")
-
-
-
-
+    
