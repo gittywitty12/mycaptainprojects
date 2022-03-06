4518fb334201fd88c0dd3a7f8e8d395eeaf767a7
gittywitty12 <100278356+gittywitty12@users.noreply.github.com>
2022-03-06 14:40:17 +0530

    Create positivenum.py

---
+++ positivenum.py
@@ -0,0 +1,22 @@
+#positive numbers extraction from a list
+l1=int(input("Enter first number: "))
+l2=int(input("Enter second number: "))
+l3=int(input("Enter third number: "))
+l4=int(input("Enter fourth number: "))
+l5=int(input("Enter fifth number: "))
+l6=int(input("Enter sixth number: "))
+list1=[]
+if l1>0:
+    list1.append(l1)
+if l2>0:
+    list1.append(l2)
+if l3>0:
+    list1.append(l3)
+if l4>0:
+    list1.append(l4)
+if l5>0:
+    list1.append(l5)
+if l6>0:
+    list1.append(l6)
+
+print(list1)
